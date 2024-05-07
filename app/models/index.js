49ff4f1e3752b982967ea30fa45a8f72f42a2902
xRatchapholx <ratchaphol.wira@northbkk.ac.th>
2024-05-07 15:17:56 +0700

    first commit

---
+++ app/models/index.js
@@ -0,0 +1,28 @@
+const config = require("../config/db.js");
+
+const dataType = require("sequelize");
+const sequelize = new dataType(
+  config.DB,
+  config.USER,
+  config.PASSWORD,
+  {
+    host: config.HOST,
+    dialect: config.dialect
+  }
+);
+
+const db = {};
+
+db.dataType = dataType;
+db.sequelize = sequelize;
+
+db.employee = require("./employee.model.js")(sequelize, dataType);
+db.setting = require("./setting.model.js")(sequelize, dataType);
+
+// one-to-one
+db.employee.hasOne(db.setting, {
+  onDelete: 'CASCADE'
+});
+db.setting.belongsTo(db.employee);
+
+module.exports = db;
